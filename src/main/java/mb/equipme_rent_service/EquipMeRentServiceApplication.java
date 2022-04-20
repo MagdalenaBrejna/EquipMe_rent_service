0d40497262157a70d739645c3d8d8021d2c7d672
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-04-20 21:45:13 +0200

    feat: Initial commit

---
+++ src/main/java/mb/equipme_rent_service/EquipMeRentServiceApplication.java
@@ -0,0 +1,13 @@
+package mb.equipme_rent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipMeRentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipMeRentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mb/equipme_rent_service/EquipMeRentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.equipme_rent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipMeRentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
